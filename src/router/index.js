334b61770b1196fd8049a32b9382c2cfc840552d
Ethan <langlois.ethan09@gmail.com>
2026-03-28 16:44:52 +0100

    premier commit

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import NotFoundView from "../views/NotFoundView.vue";
+
+const routes = [
+  { path: "/", component: HomeView },
+  { path: "/:pathMatch(.*)*", component: NotFoundView }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
