8ee6d7c4f7bd1d121e22f6b69198400dbdd6e951
Ethan <langlois.ethan09@gmail.com>
2026-03-29 17:15:51 +0200

    ajout footer avec icônes réseaux sociaux en svg et date de mise à jour

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
   <div>
     <AppHeader />
     <RouterView />
+    <AppFooter />
   </div>
 </template>
 
 <script setup>
 import AppHeader from './components/AppHeader.vue';
+import AppFooter from './components/AppFooter.vue';
 </script>
 
 <style>
