678097211635167efbd71403229ff697b41acc79
Ethan <langlois.ethan09@gmail.com>
2026-03-29 19:07:03 +0200

    ajout image et amélioration contenu page 404

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,7 +1,14 @@
 <template>
     <div>
-        <h1>404</h1>
-        <p>Cette page n'existe pas.</p>
+        <h1>OUPS 404 !</h1>
+        <img src="/404.svg" alt="Image d'homme perdu">
+        <p>Tu t'es visiblement perdu, parce que je ne trouve pas la page que tu cherches.</p>
         <RouterLink to="/">Retour à l'accueil</RouterLink>
     </div>
 </template>
+
+<script setup>
+
+// Cette page sert à rediriger vers l'accueil en cas d'url inexistant
+
+</script>
